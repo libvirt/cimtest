d244324487e9ce9e78c3cc8791515370fba0d7b5
Guolian Yun <yunguol@cn.ibm.com>
2008-04-08 22:13:43 -0700

    [TEST] fix AllocationUnits Mismatch in VSSD.04
    Signed-off-by: Guolian Yun <yunguol@cn.ibm.com>

--- suites/libvirt-cim/cimtest/VSSD/04_vssd_to_rasd.py
+++ suites/libvirt-cim/cimtest/VSSD/04_vssd_to_rasd.py
@@ -105,7 +105,7 @@ def init_list(virt):
     memrasd = {
                "InstanceID"  : '%s/%s' %(test_dom, "mem"), 
                "ResourceType" : 4, 
-               "AllocationUnits" : "MegaBytes",
+               "AllocationUnits" : "KiloBytes",
                "VirtualQuantity" : (test_mem * 1024), 
                "CreationClassName": get_typed_class(virt, 'MemResourceAllocationSettingData')
               }
