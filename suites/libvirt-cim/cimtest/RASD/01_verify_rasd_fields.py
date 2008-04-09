838fc8c520a005395b05002b8342a223b1316618
Guolian Yun <yunguol@cn.ibm.com>
2008-04-08 22:10:05 -0700

    [TEST] fix RASD.01 AllocationUnits Mismatch
    Signed-off-by: Guolian Yun <yunguol@cn.ibm.com>

--- suites/libvirt-cim/cimtest/RASD/01_verify_rasd_fields.py
+++ suites/libvirt-cim/cimtest/RASD/01_verify_rasd_fields.py
@@ -97,7 +97,7 @@ def init_list(xml, disk, virt="Xen"):
     memrasd = {
                "InstanceID"  : '%s/%s' %(test_dom, "mem"), \
                "ResourceType" : 4, \
-               "AllocationUnits" : "MegaBytes",\
+               "AllocationUnits" : "KiloBytes",\
                "VirtualQuantity" : (test_mem * 1024), \
                "CreationClassName": get_typed_class(virt, 'MemResourceAllocationSettingData')
               }
