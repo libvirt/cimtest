9fd3b14022b1de03f5b8c80797b200b7911da5fa
Guolian Yun <yunguol@cn.ibm.com>
2008-09-25 23:05:47 -0700

    [TEST] #4 Fix HostSystem-01 with sblim base provider installed
    Signed-off-by: Guolian Yun <yunguol@cn.ibm.com>

--- suites/libvirt-cim/cimtest/HostSystem/01_enum.py
+++ suites/libvirt-cim/cimtest/HostSystem/01_enum.py
@@ -30,6 +30,7 @@ from XenKvmLib import enumclass
 from XenKvmLib.classes import get_typed_class
 from VirtLib import live
 from VirtLib import utils
+from XenKvmLib.common_util import check_sblim
 from CimTest.Globals import logger, CIM_ERROR_ENUMERATE
 from XenKvmLib.const import do_main
 from CimTest.ReturnCodes import PASS, FAIL
@@ -43,10 +44,29 @@ def main():
    
     status = FAIL
     keys = ['Name', 'CreationClassName']
+    name = get_typed_class(options.virt, 'HostSystem')
+    
+    ret, linux_cs = check_sblim(options.ip, options.virt)
     try:
         hs = enumclass.enumerate(options.ip, 'HostSystem', keys, options.virt)
-        name = get_typed_class(options.virt, 'HostSystem')
-        
+    except Exception, details:
+        logger.error("%s %s: %s" % (CIM_ERROR_ENUMERATE, name, details))
+        status = FAIL
+
+    if ret == PASS:
+        if len(hs) != 0:
+            logger.error("Unexpected instance returned")
+            return FAIL
+        else:
+            if linux_cs.CreationClassName != 'Linux_ComputerSystem'\
+              or linux_cs.Name != host:
+                logger.error("Exp Linux_ComputerSystem, got %s" \
+                             % linux_cs.CreationClassName)
+                logger.error("Exp %s, got %s" % (host, system.Name))
+                return FAIL
+            else:
+                return PASS
+    else:
         if len(hs) != 1:
             logger.error("Expected 1 %s instance returned" % name)
             return FAIL
@@ -60,10 +80,6 @@ def main():
         else:
             logger.info("%s is %s" % (name, host))
             status = PASS
-
-    except Exception, details:
-        logger.error("%s %s: %s" % (CIM_ERROR_ENUMERATE, name, details))
-        status = FAIL
 
     return status
 
